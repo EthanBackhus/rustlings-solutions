1abf8b9462d67a9c1ca412fd8732fda3ecdb8687
Ethan Backhus <backhusethan@gmail.com>
2026-01-15 17:26:25 -0500

    finished, primitives, vecs, and started working on move semantics

--- solutions/04_primitive_types/primitive_types6.rs
+++ solutions/04_primitive_types/primitive_types6.rs
@@ -6,11 +6,9 @@ fn main() {
 mod tests {
     #[test]
     fn indexing_tuple() {
-        let numbers: (i32, i32, i32) = (1, 2, 3);
+        let numbers = (1, 2, 3);
 
-        // TODO: Use a tuple index to access the second element of `numbers`
-        // and assign it to a variable called `second`.
-        // let second = ???;
+        // Tuple indexing syntax.
         let second = numbers.1;
 
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
